cd80ab4925c80f307d18c350270706b5bd2c0d1b
David Woodward <davidwoodward@Davids-MacBook-Pro.local>
2017-05-17 16:28:43 -0500

    david

--- AS_Files/assets/js/firebase_getdates.js
+++ AS_Files/assets/js/firebase_getdates.js
@@ -2,9 +2,6 @@ var user = firebase.auth().currentUser;
 var name = user.displayName;
 
     if (user != null){
-      const dbRefObject = firebase.database().ref().child(name)
+      const dbRefObject = firebase.database().ref().child(name);
       dbRefObject.on('value', snap => consle.log(snap.val()));
-
-
     }
-});
