c5be28a680dcd846592d58452046a9c2f7ba583b
David Woodward <davidwoodward@Davids-MacBook-Pro.local>
2017-05-17 17:47:16 -0500

    david

--- AS_Files/assets/js/firebase_getdates.js
+++ AS_Files/assets/js/firebase_getdates.js
@@ -9,6 +9,17 @@ var user = firebase.auth().currentUser;
         snapshot.forEach(function(childSnapshot){
           var childKey = childSnapshot.key;
           console.log("Dates Loaded: " + childKey);
+
         });
       });
     }
+
+console.log(childKey(2));
+/*for (childKey(n)
+  var s = document.createElement("input");
+    s.setAttribute('type',"checkbox");
+    s.setAttribute('value',"");
+
+    f.appendChild(s);
+
+    document.getElementsByTagName('body')[0].appendChild(f);*/
