fcfe8b1f5614a15a150e6267f6537dd94ac9abe8
David Woodward <davidwoodward@Davids-MacBook-Pro.local>
2017-05-17 16:47:55 -0500

    david

--- AS_Files/assets/js/firebase_getdates.js
+++ AS_Files/assets/js/firebase_getdates.js
@@ -3,7 +3,9 @@ var name = user.displayName
 
     if (user != null){
       const dbRefObject = firebase.database().ref().child(name);
-      const dbRefList = dbRefObject.child(name)
+      const dbRefList = firebase.database().ref().child(name);
+      console.log("object");
       dbRefObject.on('value', snap => console.log(snap.val()));
+      console.log("list");
       dbRefList.on('value', snap => console.log(snap.val()));
     }
