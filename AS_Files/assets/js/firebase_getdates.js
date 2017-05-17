2af74d916eed08769653be49eb0bb76ada4ff52f
David Woodward <davidwoodward@Davids-MacBook-Pro.local>
2017-05-17 17:33:52 -0500

    david

--- AS_Files/assets/js/firebase_getdates.js
+++ AS_Files/assets/js/firebase_getdates.js
@@ -1,13 +1,14 @@
 var user = firebase.auth().currentUser;
-var name = user.displayName
+
 
     if (user != null){
+      var name = user.displayName
       const dbRefObject = firebase.database().ref().child(name);
       dbRefObject.on('value', snap => console.log(snap.val()));
       dbRefObject.on('value', function(snapshot){
         snapshot.forEach(function(childSnapshot){
           var childKey = childSnapshot.key;
-          console.log("Key: " + childKey);
+          console.log("Dates Loaded: " + childKey);
         });
       });
     }
