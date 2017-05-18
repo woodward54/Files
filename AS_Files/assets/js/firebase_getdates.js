4b9dba676875b853611fecc574e3a43fb20a2763
David Woodward <davidwoodward@Davids-MacBook-Pro.local>
2017-05-17 20:02:57 -0500

    david

--- AS_Files/assets/js/firebase_getdates.js
+++ AS_Files/assets/js/firebase_getdates.js
@@ -4,7 +4,6 @@ var user = firebase.auth().currentUser;
     if (user != null){
       var name = user.displayName
       const dbRefObject = firebase.database().ref().child(name);
-      dbRefObject.on('value', snap => console.log(snap.val()));
       dbRefObject.on('value', function(snapshot){
         snapshot.forEach(function(childSnapshot){
           var childKey = childSnapshot.key;
@@ -18,7 +17,6 @@ function createdate(childKey) {
   var i = document.createElement("input");
   i.setAttribute('type',"checkbox");
   i.setAttribute('name',childKey);
-  i.appendChild(i);
   document.getElementById('dates')[0].appendChild(i);
 
 }
