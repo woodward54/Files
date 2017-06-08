61f68a68f5eeb645ed1eac82041cd1cfc9cc3ce2
David Woodward <davidwoodward@Davids-MacBook-Pro.local>
2017-06-08 14:27:40 -0500

    david

--- AS_Files/assets/js/firebase_getdates.js
+++ AS_Files/assets/js/firebase_getdates.js
@@ -8,7 +8,7 @@ firebase.auth().onAuthStateChanged(user => {
           var childKey = childSnapshot.key;
 
 
-          const dbRefObject2 = firebase.database().ref().child(name).childKey;
+          const dbRefObject2 = firebase.database().ref().child(name).child(childKey);
           dbRefObject2.once('value', function(snapshot){
             snapshot.forEach(function(childSnapshot){
               var flight = childSnapshot.key;
